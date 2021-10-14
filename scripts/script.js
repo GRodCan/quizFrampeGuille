4ec03fbf3562a94855d3332f47992a18b6e86b5d
GRodCan <g.rodcan@outlook.es>
2021-10-14 14:23:59 +0200

    make divModes css, and animation

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,10 @@ const playDiv=()=>{
       return document.getElementById("start").removeChild(document.getElementById("play"))
     } else{
     return document.getElementById("start").innerHTML += `<div id="play">
-    <input type="text" name="" id="" placeholder="Player Name">
-    <button>10 PREGUNTAS</button><button>25 PREGUNTAS</button><button>50 PREGUNTAS</button>
+      <p>Name:</p><input type="text" name="" id="playerName" placeholder="Player Name">
+      <div id="buttonsMode">
+        <button>10 PREGUNTAS</button><button>25 PREGUNTAS</button><button>50 PREGUNTAS</button>
+      </div>
     </div>`}
   }
   
